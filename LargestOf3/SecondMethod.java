206b771cbecadff20201e58f0014d7265e412aeb
Sneha1023 <136509614+Sneha1023@users.noreply.github.com>
2024-04-27 22:54:09 +0530

    Add files via upload

---
+++ LargestOf3/SecondMethod.java
@@ -0,0 +1,24 @@
+package LargestOf3;
+
+import java.util.Scanner;
+
+public class SecondMethod {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		int max = 0;
+		if ( a> b) {
+			max = a;
+		}
+		else {
+			max = b;
+		}
+		if (c> max) {
+			max = c;
+		}
+		System.out.println(max);
+	}
+}
