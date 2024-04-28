6a1562ad88bf71e5fe9926296b0606e377885aae
Sneha1023 <136509614+Sneha1023@users.noreply.github.com>
2024-04-29 01:38:39 +0530

    Add files via upload

---
+++ CharacterCaseCheck/Main.java
@@ -0,0 +1,21 @@
+package CharacterCaseCheck;
+
+import java.util.Scanner;
+
+public class Main {
+		
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			char ch = sc.next().trim().charAt(0);
+			// .trim() removes the extra space from the string
+			// .charAt(0) means give me the character at 0 index from the string
+
+			if (ch >= 'a' && ch <= 'z') { // && is used to check both the situation is true
+				System.out.println("lowercase");
+			} else {
+				System.out.println("UPPERCASE");
+			}
+
+			System.out.println(ch);
+		}
+}
---
+++ FibonacciSeries/Main.java
@@ -0,0 +1,5 @@
+package FibonacciSeries;
+
+public class Main {
+
+}
